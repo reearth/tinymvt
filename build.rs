20bb690ba857c15c12a970d3d04e8b47e4b3fdbc
Taku Fukada <naninunenor@gmail.com>
2024-10-22 06:26:09 +0900

    init

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::{env, io::Result, path::Path};
+
+fn main() -> Result<()> {
+    // build vector_tile.rs from vector_tile.proto
+    if !Path::new("src/vector_tile.rs").exists() {
+        let prev_out_dir = env::var("OUT_DIR");
+
+        env::set_var("OUT_DIR", "src/");
+        prost_build::compile_protos(&["src/vector_tile.proto"], &["src"])?;
+
+        if let Ok(prev) = prev_out_dir {
+            env::set_var("OUT_DIR", prev);
+        }
+    }
+    Ok(())
+}
